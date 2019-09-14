fb4792f1b0d4d191f0ca4eff25a1cd773dac3ecc
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-14 16:31:25 +0300

    make better

--- src/app/lodash/lodash.component.ts
+++ src/app/lodash/lodash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-lodash',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lodash.component.css']
 })
 export class LodashComponent implements OnInit {
+  arr = [{name: 'Ivan', id: 1}, {name: 'Petro', id: 0}, {name: 'Vasul', id: 3}, {name: 'Taras', id: 4}];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(_.add(5, 9));
   }
 
 }
