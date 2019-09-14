f0c9276fbde5d4d3d04a4ab069c256f66f46dccd
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-14 16:27:30 +0300

    collections mode

--- src/app/lodash/lodash.component.ts
+++ src/app/lodash/lodash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-lodash',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lodash.component.css']
 })
 export class LodashComponent implements OnInit {
+  arr = [{name: 'Ivan', id: 1}, {name: 'Petro', id: 0}, {name: 'Vasul', id: 3}, {name: 'Taras', id: 4}];
 
   constructor() { }
 
   ngOnInit() {
+    // console.log(_.each(this.arr, item => console.log(item.id + 5)));
+    // console.log(this.arr);
+    // console.log(_.map(this.arr, item => {
+    //   return (item.id + 5);
+
+    // }));
+    // console.log(this.arr);
+    // console.log(_.map(this.arr, 'name'));
+    // console.log(this.arr);
+    // console.log(_.filter(this.arr, 'id'));
+    // console.log(this.arr);
+    // console.log(_.find(this.arr, (item) => item.id === 0));
+    // console.log(this.arr);
+    // console.log(_.findLast(this.arr, 'name'));
+    // console.log(this.arr);
+    // console.log(_.reject(this.arr, 'id'));
+    // console.log(this.arr);
+    // console.log(_.shuffle(this.arr));
+    // console.log(this.arr);
+    // console.log(_.size(this.arr));
+    // console.log(this.arr);
+    console.log(_.sortBy(this.arr, [o => o.id]));
+    console.log(this.arr);
+
+
   }
 
 }
